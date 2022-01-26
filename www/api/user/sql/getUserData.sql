2c6707749279ea0acc614d526cdf263e65e76816
Серебряная Нить EnvyBook <serebnit@gmail.com>
2022-01-26 21:38:19 +0300

    Update getUserData.sql

--- www/api/user/sql/getUserData.sql
+++ www/api/user/sql/getUserData.sql
@@ -6,7 +6,7 @@ SELECT
     users.`photo`,
     users.`strava_id`,
     users.`register_date`,
-    COUNT(SELECT deadline FROM referees WHERE id_user=users.id AND deadline >= NOW() AND created_at <= NOW() LIMIT 1) as is_referee
+    (SELECT 1 FROM referees WHERE id_user=users.id AND deadline >= NOW() AND created_at <= NOW() LIMIT 1) as is_referee
 FROM
     `users`
 WHERE
